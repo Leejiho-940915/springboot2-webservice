c5b9b982f7c6890405fc98fd67a54ca6afb370d8
Leejiho <wlgh4585@daum.net>
2022-10-26 14:01:01 +0900

    2022-10-26

--- src/main/java/web/HelloController.java
+++ src/main/java/web/HelloController.java
@@ -1,14 +1,19 @@
 package web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import web.dto.HellohResponseDto;
 
 @RestController // 1.
 public class HelloController {
 
-    @GetMapping("/hello") // 2.
-    public String hello() {
-        return "hello";
+    @GetMapping("/hello/dto") // 2.
+    public HellohResponseDto helloDto(
+            @RequestParam("name") String name,
+            @RequestParam("amount") int amount
+    ) {
+        return new HellohResponseDto(name, amount);
     }
 }
 
@@ -20,4 +25,9 @@ public class HelloController {
 // - HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 준다.
 // - 예전에는 @RequestMapping(method = RequestMethod.GET)으로 사용되었다.
 // - 이 프로젝트는 /hello로 요청이 오면 문자열 hello를 반환하는 기능을 가지게 된다.
+//
+// 3. @RequestParam
+// - 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+// - 여기서는 외부에서 name (@RequestParam("name")) 이란 이름으로 넘긴 파라미터를
+//   메소드 파라미터 name(String name)에 저장하게 된다.
 
--- src/test/java/com/book/springboot/HelloControllerTest.java
+++ src/test/java/com/book/springboot/HelloControllerTest.java
@@ -6,26 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import web.HelloController;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) // 1.
-@WebMvcTest(controllers = HelloController.class) // 2.
+//@WebMvcTest(controllers = HelloController.class) // 2.
+@WebMvcTest
 public class HelloControllerTest {
 
     @Autowired // 3.
     private MockMvc mvc; // 4.
 
     @Test
-    public void helloReturn() throws Exception {
+    public void hello_Return() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello")) // 5.
                 .andExpect(status().isOk()) // 6.
                 .andExpect(content().string(hello)); // 7.
+    }
+
+    @Test
+    public void helloDto_Return() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount))) // 1.
+                .andExpect(status().isOk()) // 2.
+                .andExpect((ResultMatcher) jsonPath("$.name", is(name)))
+                .andExpect((ResultMatcher) jsonPath("$.amount", is(amount)));
     }
 }
 
