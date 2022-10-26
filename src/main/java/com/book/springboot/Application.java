6452d9a1a600ea607a40c07f29e2ee00064a6d21
Leejiho <wlgh4585@daum.net>
2022-10-26 11:04:53 +0900

    test

--- src/main/java/com/book/springboot/Application.java
+++ src/main/java/com/book/springboot/Application.java
@@ -8,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+
+        System.out.println("");
     }
 
 
